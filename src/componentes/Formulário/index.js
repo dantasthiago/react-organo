0a8c56fe1cb2995cfaa10ce74ec534e7bad8efd8
dantasthiago <thiago_dantas88@hotmail.com>
2022-08-04 10:52:50 -0300

    aula 2 finalizada

--- src/componentes/Formulário/index.js
+++ src/componentes/Formulário/index.js
@@ -1,13 +1,35 @@
 import "./Formulario.css";
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botão';
 const Formulario = () => {
+
+	const times = [
+		'Programação',
+		'Front-End',
+		'Data Sciente',
+		'DevOps',
+		'Ux e Design',
+		'Mobile',
+		'Inovação e Gestão'
+	]
+
+	const aoSalvar = (evento) => {
+		evento.preventDefault();
+		console.log('form foi submetido');
+	}
+
 	return (
 		<section className='formulario'>
-			<form>
+			<form onSubmit={aoSalvar}>
         <h2>Preencha os dados para cirar o card do colaborador</h2>
-				<CampoTexto label='Nome' placeholder='Digite sei nome' />
-				<CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+				<CampoTexto obrigatorio label='Nome' placeholder='Digite sei nome' />
+				<CampoTexto obrigatorio label='Cargo' placeholder='Digite seu cargo' />
 				<CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+				<ListaSuspensa obrigatorio label='Time' itens={times}/>
+				<Botao>
+					Criar card
+				</Botao>
 			</form>
 		</section>
 	);
